fff2ed67141c9cddc06fb8dffcd6cfad883711cd
Vaino Liukko <vaino.liukko@tuni.fi>
2023-02-03 10:28:16 +0200

    Changed the hello world text to ascii

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const figlet = require('figlet');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  figlet.text('Hello World!', (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
